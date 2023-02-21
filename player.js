773e26cba872fcc1fcad0e402b66b0f25475dd31
Leonardo do Espirito Santo de Santana <leonardosantana28@yahoo.com.br>
2023-02-21 17:29:45 -0300

    first commit

---
+++ player.js
@@ -0,0 +1,28 @@
+export class Player {
+    constructor(game){
+        this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = this.game.height - this.height;
+        this.vy = 0;
+        this.image = document.getElementById('player');
+        this.speed = 0;
+        this.maxSpeed = 10; //Velocidade em pixels por /frame
+    }
+    update(input){
+        // Movimento horizontal
+        this.x += this.speed;
+
+        if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
+        if (this.x < 0) this.x = 0;
+        if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+        // Movimento vertical
+        this.y += this.vy;
+    }
+    draw(context){
+       context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+}
